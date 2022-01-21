7849778929780c605be6aa9b5686f752ee8f77ff
ZJULeBron23 <98151412+ZJULeBron23@users.noreply.github.com>
2022-01-21 16:46:08 +0800

    Add files via upload
    add file for test.

---
+++ demo0.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // bool ret0 = 1;
+  // int ret1 = true;
+  // int ret2 = true;
+  // int ret = ret1 & ret0 & ret2;
+  // cout << ret;
+  // int a = 10;
+  // a *= 10;
+  // cout << a;
+  int ret = true;
+
+  ret &= true;
+  cout << ret;
+}
